df47925f8752e632270ab32668f416b19e7ae20b
Guillaume Plante <tigui.plante@icloud.com>
2025-02-24 11:42:22 -0500

    Updated Build Script

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,15 +19,19 @@ NC='\033[0m' # No Color
 GREEN='\033[0;32m'
 
 # variables for cmd arguments
-ASYNC_OPT=0
+GUI_OPT=0
 CLEAN_OPT=0
-DEBUG_OPT=0
-RUN_OPT=0
+TARGET_OPT=0
 INCREMENTAL_OPT=0
-
-UPDATE_VERSION_OPT=0
-DEPLOYIMAGE_OPT=0
-GETVERSION_OPT=0
+NOMAKE_OPT=0
+BUILD_TARGET="Release"
+GUI_FEATURE="OFF"
+WEBUI_FEATURE="ON"
+STACKTRACE_FEATURE="ON"
+DBUS_FEATURE="ON"
+TESTING_FEATURE="OFF"
+VERBOSE_CONFIGURE_FEATURE="OFF"
+SYSTEMD_FEATURE="OFF"
 
 SCRIPT_PATH=$(realpath "$BASH_SOURCE")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
@@ -49,9 +53,16 @@ if [[ ! -f "$CMAKELIST_FILE" ]]; then
    exit 1
 fi
 
-$VERSION_SCRIPT
-
-pushd $ROOT_DIR > /dev/null
+# Usage function
+usage() {
+    echo "Usage: $0 [options]"  
+    echo "  -t, --target <Debug|Release>    Build target"
+    echo "  -c, --clean                     Clean"
+    echo "  -g, --gui                       Enable GUI"
+    echo "  -n, --nomake                    No make, just configure"
+    echo "  -h, --help                      Show this help message"
+    exit 0
+}
 
 # =========================================================
 # function:     logs functions
@@ -80,16 +91,73 @@ log_error() {
 }
 
 
+# Parse script arguments
+for arg in "$@"; do
+    case $arg in
+        -g|--gui)
+            GUI_OPT=1
+            GUI_FEATURE="ON"
+            ;;
+        -t|--target)
+            TARGET_OPT=1
+            shift # Shift past the version
+            if [[ $# -gt 1 ]]; then
+                TMP_TARGET="$2"
+                if [[ "$TMP_TARGET" == "debug" || "$TMP_TARGET" == "release" ]]; then
+                    BUILD_TARGET="$TMP_TARGET"
+                else
+                    log_error "invalid build target, use debug or release"
+                fi
+            fi
+            ;;
+        -c|--clean)
+            CLEAN_OPT=1
+            ;;
+        -n|--nomake)
+            NOMAKE_OPT=1
+            ;;
+        -h|--help)
+            usage
+            ;;
+    esac
+done
+
+
+
+pushd $ROOT_DIR > /dev/null
+
+
+
 if ! which cmake; then
     log_error "missing cmake"
 fi
 
+log_info "======================================================"
+log_info "BUILD_TARGET          $BUILD_TARGET"
+log_info "GUI                   $GUI_FEATURE"
+log_info "WEBUI                 $WEBUI_FEATURE"
+log_info "STACKTRACE            $STACKTRACE_FEATURE"
+log_info "DBUS                  $DBUS_FEATURE"
+log_info "TESTING               $TESTING_FEATURE"
+log_info "VERBOSE_CONFIGURE     $VERBOSE_CONFIGURE_FEATURE"
+log_info "SYSTEMD               $SYSTEMD_FEATURE"
+log_info "PARALLEL              $(nproc)"
+log_info "======================================================"
 
-cmake -B build -DCMAKE_BUILD_TYPE=Release -DGUI=OFF
+cmake -B build -DCMAKE_BUILD_TYPE=$BUILD_TARGET -DGUI=$GUI_FEATURE -DWEBUI=$WEBUI_FEATURE -DSTACKTRACE=$STACKTRACE_FEATURE -DDBUS=$DBUS_FEATURE -DTESTING=$TESTING_FEATURE -DVERBOSE_CONFIGURE=$VERBOSE_CONFIGURE_FEATURE -DSYSTEMD=$SYSTEMD_FEATURE
 
 if [[ $? -ne 0 ]]; then
     log_error "cmake error"
 fi
+
+
+
+if [[ $NOMAKE_OPT -eq 1 ]]; then
+    log_info "nomake: EXIT!"
+    exit 0
+fi
+
+$VERSION_SCRIPT
 
 cmake --build build --parallel $(nproc)
 if [[ $? -ne 0 ]]; then
